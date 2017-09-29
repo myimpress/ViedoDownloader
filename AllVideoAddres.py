c6b770740f9c260198f8e7591f2cf460235f2b62
wen <myimpress@users.noreply.github.com>
2017-09-29 10:16:06 +0800

    Update AllVideoAddres.py

--- AllVideoAddres.py
+++ AllVideoAddres.py
@@ -3,34 +3,31 @@ import os
 
 
 
-filename = "1003425004.html"#生成的html
-path = 'D:/迅雷下载/视频任务组_20170927_1722/'#下载后视频存在路径
+filename = "1004014054.html"#生成的html
+path = 'D:/迅雷下载/视频任务组_20170927_1730/'#下载后视频存在路径
 
 f=open(filename,'r',encoding='utf-8')
 LessonName=re.findall(r'(100.*?)<',f.read())
 dic = {}
-
+i=0
 for tmp in LessonName:
+    i = i+1
+
     url_name = tmp.split(">")
     url = re.findall(r'(100.*?.flv)',url_name[0])[0]
     name = url_name[1]
-    dic[url] = name
+    dic[url] = str(i)+name
 
 f.close()
 
 
 
-
 movie_name = os.listdir(path)
-i=0
+
 for temp in movie_name:
-    i = i+1
     new_name = dic[temp]+".flv"
-    os.rename(path+temp,path+str(i)+new_name)
+    os.rename(path+temp,path+new_name)
 
 print("done")
-
-
-
 
 
